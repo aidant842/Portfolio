a726b2f8513119f76e8fdeeb4d344d96fc5b128d
aidant842 <aidant842@gmail.com>
2021-01-21 15:51:18 +0000

    deployment with aws attempt 2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,7 +138,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'aidan-thorpe-portfolio'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # static and media files
